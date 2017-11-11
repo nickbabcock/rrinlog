ebc426939be31a8f91caef8c93f880bda73a42ea
Nick Babcock <nbabcock19@hotmail.com>
2017-11-10 23:59:35 -0500

    More resiliant error handling

--- rrinlog/src/main.rs
+++ rrinlog/src/main.rs
@@ -52,13 +52,12 @@ fn persist_logs(threshold: usize, db: &str) {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         buffer.push(line.unwrap());
-        if buffer.len() == threshold {
-            insert_buffer(&conn, &buffer);
-            buffer.clear();
+        if buffer.len() >= threshold {
+            insert_buffer(&conn, &mut buffer);
         }
     }
 
-    insert_buffer(&conn, &buffer)
+    insert_buffer(&conn, &mut buffer)
 }
 
 fn dry_run() {
@@ -67,6 +66,8 @@ fn dry_run() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         match parser::parse_nginx_line(line.unwrap().as_str()) {
+            // Both Ok and Err branches halt writing if the line can't be ouput.
+            // For instance, this occurs when rrinlog output is piped to head
             Ok(log) => if writeln!(&mut handle, "line: {}", log).is_err() {
                 break;
             },
@@ -77,30 +78,46 @@ fn dry_run() {
     }
 }
 
-fn insert_buffer(conn: &SqliteConnection, buffer: &[String]) {
+fn insert_buffer(conn: &SqliteConnection, buffer: &mut Vec<String>) {
     use rrinlog_core::schema::logs;
     use diesel::result::Error;
 
     let start = Utc::now();
-    conn.transaction::<_, Error, _>(|| {
-        for l in buffer {
+    let init_len = buffer.len();
+    let trans = conn.transaction::<_, Error, _>(|| {
+        for l in buffer.drain(..) {
             match parser::parse_nginx_line(l.as_str()) {
                 Ok(ng) => {
-                    diesel::insert(&ng)
-                        .into(logs::table)
-                        .execute(conn)
-                        .expect("to insert records");
+                    // If we can't insert our parsed log then our schema not be representative of
+                    // the data. The error shouldn't be a sqlite write conflict as that is checked
+                    // at the transaction level, but since I'm not a better man I won't assume the
+                    // cause of the error. Instead of panicking, discard the line and log the error.
+                    if let Err(ref e) = diesel::insert(&ng).into(logs::table).execute(conn) {
+                        error!("Insertion error: {}", e)
+                    }
                 }
-                Err(ref e) => error!("{}", e.display_chain()),
+
+                // If we can't parse a line, yeah that sucks but it's bound to happen so discard
+                // the line after it's logged for the attentive sysadmin
+                Err(ref e) => error!("Parsing error: {}", e.display_chain()),
             }
         }
         Ok(())
-    }).expect("to complete transaction");
+    });
+
+    // If SQLite is unable to acquire needed locks for the write to succeed (for instance if WAL
+    // mode is off and there is a reader), the transaction will error. Don't panic as rrinlog is
+    // supposed to be a long lived application. We'll simply wait for the next line to try again
+    if let Err(ref e) = trans {
+        error!("Unable to complete transaction: {}", e);
+        return;
+    }
+
     let end = Utc::now();
     let dur = end.signed_duration_since(start);
     info!(
         "Parsing and inserting {} records took {}us",
-        buffer.len(),
+        init_len,
         dur.num_microseconds().unwrap()
     );
 }

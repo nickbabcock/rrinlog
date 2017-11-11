e330f36beec009f903bcbeca5e9746a915994e82
Nick Babcock <nbabcock19@hotmail.com>
2017-11-11 03:44:22 -0500

    Add query for sites

--- rrinlog-server/src/dao.rs
+++ rrinlog-server/src/dao.rs
@@ -9,6 +9,13 @@ pub struct BlogPost {
     pub views: i32,
 }
 
+#[derive(PartialEq, Debug, Queryable)]
+pub struct Sites {
+    pub ep: i64,
+    pub host: String,
+    pub views: i32,
+}
+
 static BLOG_POST_QUERY: &'static str = r#"
 SELECT referer,
        Count(*) AS views
@@ -24,11 +31,29 @@ GROUP  BY referer
 ORDER  BY views DESC
 "#;
 
-
 pub fn blog_posts(conn: &SqliteConnection, range: &Range, ip: &str) -> QueryResult<Vec<BlogPost>> {
     let query = sql::<(Text, Integer)>(&BLOG_POST_QUERY)
         .bind::<BigInt, _>(range.from.timestamp())
         .bind::<BigInt, _>(range.to.timestamp())
         .bind::<Text, _>(ip);
     LoadDsl::load::<BlogPost>(query, conn)
+}
+
+pub fn sites(conn: &SqliteConnection, range: &Range, interval_ms: i64) -> QueryResult<Vec<Sites>> {
+    let interval_s = interval_ms / 1000;
+    let qs = format!(r#"
+SELECT (epoch / {}) * {} AS nep,
+       host,
+       Count(*) AS views
+FROM   logs
+WHERE  host LIKE "%nbsoftsolutions.com"
+       AND epoch >= ?
+       AND epoch < ?
+GROUP BY epoch / ({}),
+         host
+"#, interval_s, interval_ms, interval_s);
+    let query = sql::<(BigInt, Text, Integer)>(&qs)
+        .bind::<BigInt, _>(range.from.timestamp())
+        .bind::<BigInt, _>(range.to.timestamp());
+    LoadDsl::load::<Sites>(query, conn)
 }

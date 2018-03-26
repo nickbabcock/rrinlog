3ffc8deb3fad0c42ecb6f92c7ea955723e5d7026
Nick Babcock <nbabcock19@hotmail.com>
2018-03-25 22:46:17 -0400

    Migrate to criterion for stable benchmarking

--- src/lib.rs
+++ src/lib.rs
@@ -14,18 +14,3 @@ extern crate regex;
 pub mod models;
 pub mod parser;
 pub mod schema;
-
-#[cfg(all(feature = "unstable", test))]
-mod bench {
-    extern crate test;
-    use parser;
-
-    #[bench]
-    fn bench_parse_nginx(b: &mut test::Bencher) {
-        let line =
-            r#"127.0.0.1 - - [04/Nov/2017:13:05:35 -0500] "GET /js/embed.min.js HTTP/2.0" 200 20480 "https://nbsoftsolutions.com/blog/monitoring-windows-system-metrics-with-grafana" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36" "comments.nbsoftsolutions.com""#;
-        b.iter(|| {
-            assert!(parser::parse_nginx_line(line).is_ok());
-        });
-    }
-}
